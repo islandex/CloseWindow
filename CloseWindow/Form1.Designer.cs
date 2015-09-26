e5d26daba5af3f77e3d8943b29d217cd2cc10f80
Tomek <tomymail1@gmail.com>
2015-09-26 13:07:43 +0200

    introduced tray

--- CloseWindow/Form1.Designer.cs
+++ CloseWindow/Form1.Designer.cs
@@ -29,11 +29,158 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.numericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.logout = new System.Windows.Forms.Button();
+            this.forceLogOut = new System.Windows.Forms.Button();
+            this.shutDown = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.hideApp = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown
+            // 
+            this.numericUpDown.Location = new System.Drawing.Point(220, 59);
+            this.numericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown.Name = "numericUpDown";
+            this.numericUpDown.Size = new System.Drawing.Size(58, 20);
+            this.numericUpDown.TabIndex = 0;
+            this.numericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(216, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Akcja za:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(284, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "min";
+            // 
+            // logout
+            // 
+            this.logout.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.logout.Location = new System.Drawing.Point(28, 25);
+            this.logout.Name = "logout";
+            this.logout.Size = new System.Drawing.Size(136, 34);
+            this.logout.TabIndex = 3;
+            this.logout.Text = "Log out";
+            this.logout.UseVisualStyleBackColor = true;
+            this.logout.Click += new System.EventHandler(this.logout_Click);
+            // 
+            // forceLogOut
+            // 
+            this.forceLogOut.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.forceLogOut.Location = new System.Drawing.Point(28, 65);
+            this.forceLogOut.Name = "forceLogOut";
+            this.forceLogOut.Size = new System.Drawing.Size(136, 34);
+            this.forceLogOut.TabIndex = 4;
+            this.forceLogOut.Text = "Force Log out";
+            this.forceLogOut.UseVisualStyleBackColor = true;
+            this.forceLogOut.Click += new System.EventHandler(this.forceLogOut_Click);
+            // 
+            // shutDown
+            // 
+            this.shutDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.shutDown.Location = new System.Drawing.Point(28, 105);
+            this.shutDown.Name = "shutDown";
+            this.shutDown.Size = new System.Drawing.Size(136, 34);
+            this.shutDown.TabIndex = 5;
+            this.shutDown.Text = "ShutDown";
+            this.shutDown.UseVisualStyleBackColor = true;
+            this.shutDown.Click += new System.EventHandler(this.shutDown_Click);
+            // 
+            // restart
+            // 
+            this.restart.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.restart.Location = new System.Drawing.Point(28, 145);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(136, 34);
+            this.restart.TabIndex = 6;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // hideApp
+            // 
+            this.hideApp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.hideApp.Location = new System.Drawing.Point(28, 188);
+            this.hideApp.Name = "hideApp";
+            this.hideApp.Size = new System.Drawing.Size(136, 34);
+            this.hideApp.TabIndex = 7;
+            this.hideApp.Text = "Hide Window";
+            this.hideApp.UseVisualStyleBackColor = true;
+            this.hideApp.Click += new System.EventHandler(this.hideApp_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Close Windows";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(390, 246);
+            this.Controls.Add(this.hideApp);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.shutDown);
+            this.Controls.Add(this.forceLogOut);
+            this.Controls.Add(this.logout);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Close Windows";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button logout;
+        private System.Windows.Forms.Button forceLogOut;
+        private System.Windows.Forms.Button shutDown;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Button hideApp;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- CloseWindow/Form1.cs
+++ CloseWindow/Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+
 
 namespace CloseWindow
 {
@@ -16,5 +19,64 @@ namespace CloseWindow
         {
             InitializeComponent();
         }
+
+        [DllImport("user32.dll")]
+        public static extern int ExitWindowsEx(int operationFlag, int operationReason);
+
+        private void logout_Click(object sender, EventArgs e)
+        {
+
+            MessageBox.Show(this.numericUpDown.Value.GetType().ToString());
+            //ExitWindowsEx(0, 0);
+        }
+
+        private void forceLogOut_Click(object sender, EventArgs e)
+        {
+            //ExitWindowsEx(4, 0);
+        }
+
+        private void shutDown_Click(object sender, EventArgs e)
+        {
+            //ExitWindowsEx(1, 0);
+        }
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            //ExitWindowsEx(2, 0);
+        }
+
+        private void hideApp_Click(object sender, EventArgs e)
+        {
+            Hide();
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //this.liczba++;
+            //DateTime curDate = DateTime.Now;
+            //string czas = curDate.TimeOfDay.ToString();
+            //this.labelClock.Text = czas.Remove(8, 8);
+
+            //if (this.countDownBool == true)
+            //{
+            //    this.countDownVal--;
+            //    this.labelCountDown.Text = countDownVal.ToString();
+
+            //    if (this.countDownVal == 0)
+            //    {
+            //        Show();
+            //        this.countDownBool = false;
+            //        this.countDownSetter.Enabled = true;
+            //        this.btnCountDown.Text = "Start";
+            //        this.countDownVal = this.multiply * this.countDownSetter.Value;
+            //        this.a.controls.play();
+            //    }
+            //}
+        }                              
     }
 }
